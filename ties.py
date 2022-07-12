4f08ecca078ec49515e8632e59e4492f1e3fdd81
MemmoIdiot <dalischi@hotmail.it>
2022-07-12 14:50:27 +0200

    Add start readme and images browse button click

--- ties.py
+++ ties.py
@@ -1,9 +1,13 @@
+from time import sleep
+
+from pywinauto import application, findwindows
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+
+from WindowFinder import WindowFinder
 
 option: Options = Options()
 option.add_experimental_option("debuggerAddress", "localhost:8989")
@@ -14,14 +18,33 @@ driver: Chrome = Chrome(
     options=option
     #! chrome_options=option
 )
+
+#TODO Immagini
+
+driver.find_element(
+    By.XPATH,
+    "//button[@class='Button_button__1HmfN Button_primary__7rvYY Button_inline__3k2so Button_truncated__3pQ92 Button_icon-left__1QZEf']"
+).click()
+sleep(2)
+#! app = application.Application()
+#! w_handle = app.connect(title='Apri')
+#! window = app.window_(handle=w_handle)
+#! from pprint import pprint
+#!
+#! pprint(vars(window))
+#! #! ctrl = window['Name']
+#! #! ctrl.SetText(r'C:\Users\Memmo\Dropbox\Screenshot\2019_05_20_16_31_38.jpg')
+#! #! ctrl = window['OK']
+#! #! ctrl.Click()
+#! exit()
+
 # * title
-driver.find_element(By.XPATH,
-                    "//input[@id='title']").send_keys('Title')
+driver.find_element(By.XPATH, "//input[@id='title']").send_keys('Title')
 # * description
 driver.find_element(By.XPATH,
                     "//textarea[@id='description']").send_keys('Description')
 
-# * (🇮🇹 Cravatte e fazzoletti da taschino)
+# * category (🇮🇹 Cravatte e fazzoletti da taschino)
 driver.find_element(By.XPATH, "//input[@id='catalog_id']").click()
 driver.find_element(By.XPATH, "//div[@id='catalog-5']").click()
 driver.find_element(By.XPATH, "//div[@id='catalog-82']").click()
@@ -38,7 +61,7 @@ driver.find_element(By.XPATH, "//span[@aria-labelledby='status-2']").click()
 #TODO settare colori
 
 # * price
-driver.find_element(By.XPATH, "//input[@id='price']").send_keys(10)
+driver.find_element(By.XPATH, "//input[@id='price']").send_keys(10)  # * €
 
 #* package size: little (🇮🇹 Piccola)
 driver.find_element(By.XPATH,
